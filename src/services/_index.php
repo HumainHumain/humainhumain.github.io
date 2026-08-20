a06919e1b3949708e2a018e61da6b72b457cdcd9
Maxime Larrivée-Roy <zmotrin@gmail.com>
2026-08-20 18:00:38 -0400

    expertise

--- src/services/_index.php
+++ src/services/_index.php
@@ -6,3 +6,35 @@
 ?>
 
 <section class="splash-services">Services</section>
+
+
+
+<section class="expertise-index">
+
+	<div class="anticiper">
+		<div></div>
+		<div>Anticiper les défis et faire émerger les scénarios alternatifs</div>
+	</div>
+	<div class="saisir">
+		<div></div>
+		<div>Saisir les espoirs et les appréhensions autour d’un projet</div>
+	</div>
+	<div class="creer">
+		<div></div>
+		<div>Créer des ponts entre les silos</div>
+	</div>
+	<div class="rester">
+		<div></div>
+		<div>Rester à l’affût des nouveaux modes d’être ensemble</div>
+	</div>
+	<div class="perspectives">
+		<div></div>
+		<div>Perspectives critiques et vision macro</div>
+	</div>
+	<div class="inclusion">
+		<div></div>
+		<div>Inclusion des personnes difficiles d’accès</div>
+	</div>
+
+
+</section>
