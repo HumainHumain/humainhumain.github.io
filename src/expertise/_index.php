e47178118cbee62d79625b2e602c006de89614f3
Maxime Larrivée-Roy <zmotrin@gmail.com>
2026-08-21 17:18:25 -0400

    Expertise

--- src/expertise/_index.php
+++ src/expertise/_index.php
@@ -9,27 +9,27 @@
 
 <section class="expertise-index">
 
-	<div class="anticiper">
+	<div class="anticiper" onclick="document.location.href='#anticiper'">
 		<div></div>
 		<div>Anticiper les défis et faire émerger les scénarios alternatifs</div>
 	</div>
-	<div class="saisir">
+	<div class="saisir" onclick="document.location.href='#saisir'">
 		<div></div>
 		<div>Saisir les espoirs et les appréhensions autour d’un projet</div>
 	</div>
-	<div class="creer">
+	<div class="creer" onclick="document.location.href='#creer'">
 		<div></div>
 		<div>Créer des ponts entre les silos</div>
 	</div>
-	<div class="rester">
+	<div class="rester" onclick="document.location.href='#rester'">
 		<div></div>
 		<div>Rester à l’affût des nouveaux modes d’être ensemble</div>
 	</div>
-	<div class="perspectives">
+	<div class="perspectives" onclick="document.location.href='#perspectives'">
 		<div></div>
 		<div>Perspectives critiques et vision macro</div>
 	</div>
-	<div class="inclusion">
+	<div class="inclusion" onclick="document.location.href='#inclusion'">
 		<div></div>
 		<div>Inclusion des personnes difficiles d’accès</div>
 	</div>
@@ -39,7 +39,7 @@
 
 
 
-<section class="expertise-section">
+<section class="expertise-section" id="anticiper">
 	<h3>Anticiper les défis et faire émerger les scénarios alternatifs</h3>
 	<p><strong>Vous désirez conserver votre pertinence, votre force d’impact et vous cherchez à identifier les meilleures stratégies pour assurer la pérennité de votre organisation ?</strong></p>
 	<p>Quel que soit le contexte, <strong>HUMAIN HUMAIN</strong> contribue à la compréhension de réalités complexes grâce à une vision systémique et en considérant tous les acteur.rice.s impliqué.e.s.</p>
@@ -47,7 +47,7 @@
 </section>
 
 
-<section class="expertise-section-right">
+<section class="expertise-section-right" id="saisir">
 	<h3>Saisir les espoirs et les appréhensions autour d’un projet</h3>
 	<p><strong>Vous développez un projet qui aura un impact sur la vie de citoyen.ne.s et vous souhaitez anticiper la façon dont celui-ci sera accueilli ?</strong></p>
 	<p>Vous cherchez à identifier les leviers mais aussi les résistances à l’appropriation d’un nouvel espace public ?</p>
@@ -55,7 +55,7 @@
 </section>
 
 
-<section class="expertise-section">
+<section class="expertise-section" id="creer">
 	<h3>Créer des ponts entre les silos</h3>
 	<p><strong>En rencontrant directement les gens en face-à-face et en créant des liens de qualité, nos professionnel.le.s de l’ethnographie contribuent à la cohésion sociale et réduisent les clivages entre les différentes communautés, cultures et sous-cultures.</strong></p>
 	<p>Par leur seule présence, les ethnographes créent du lien social et assument un rôle de « passeur.euse » entre les groupes, les départements ou les organisations. Ils révèlent les cloisons invisibles, la diversité des trajectoires sociales et permettent de franchir les barrières de l’âge, de l’ethnicité, du genre ou du milieu socioéconomique.</p>
@@ -63,7 +63,7 @@
 </section>
 
 
-<section class="expertise-section-right">
+<section class="expertise-section-right" id="rester">
 	<h3>Rester à l’affût des nouveaux modes d’être ensemble</h3>
 	<p><strong>L’analyse des mouvements sociaux montre que des formes alternatives de lien social sont sans cesse créées.</strong></p>
 	<p>Pour mieux appréhender le monde contemporain actuel, les organisations doivent être sensibles à ces nouvelles façons d’exister collectivement.</p>
@@ -71,7 +71,7 @@
 </section>
 
 
-<section class="expertise-section">
+<section class="expertise-section" id="perspectives">
 	<h3>Perspectives critiques et vision macro</h3>
 	<p><strong>Vous cherchez à avoir du recul et de la perspective sur de récentes problématiques ou initiatives ?</strong></p>
 	<p>Vous êtes ouverts à un regard critique sur votre organisation afin d’être outillé pour une transformation ou une actualisation en profondeur ?</p>
@@ -79,7 +79,7 @@
 </section>
 
 
-<section class="expertise-section-right">
+<section class="expertise-section-right" id="inclusion">
 	<h3>Inclusion des personnes difficiles d’accès</h3>
 	<p><strong>Un projet ayant fait l’objet d’un accompagnement ethnographique permet d’avoir un impact social inclusif qui tienne compte de la diversité des individus constituant le tissu social. </strong></p>
 	<p>En tant que chercheur.euse.s spécialisé.e.s en recherche qualitative, nous sommes formé.e.s à entrer en relation de manière respectueuse avec chaque personne, sans imposer notre façon de penser, et en nous adaptant le plus possible aux spécificités des gens avec lesquels nous dialoguons.</p>
