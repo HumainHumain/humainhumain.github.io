2b8ada2380127cc3cecdcc2933ec8decfca5cad2
Maxime Larrivée-Roy <zmotrin@gmail.com>
2026-08-23 22:26:26 -0400

    Nous joindre

--- src/nous-joindre/_index.php
+++ src/nous-joindre/_index.php
@@ -5,4 +5,15 @@
  */
 ?>
 
-<section class="splash-nousjoindre">Nous joindre</section>
+<section class="splash-nousjoindre">Nous<br>joindre</section>
+
+<section class="nousjoindre-slideshow"></section>
+
+
+<section class="nousjoindre-form">
+	
+<iframe src="https://app.cyberimpact.com/clients/36806/subscribe-forms/8937E7A9-FB81-4FF2-BC95-A38193B049D5" width="500" height="300" frameborder="0" marginheight="0" marginwidth="0" scrolling="auto"></iframe>
+
+</section>
+
+
