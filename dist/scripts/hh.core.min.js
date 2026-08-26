794a0ba3b33f2e116eae9977e4f3c618e362ea35
Maxime Larrivée-Roy <zmotrin@gmail.com>
2026-08-26 15:26:06 -0400

    Pastilles 75%

--- dist/scripts/hh.core.min.js
+++ dist/scripts/hh.core.min.js
@@ -7,7 +7,7 @@
 	██║  ██║╚██████╔╝██║ ╚═╝ ██║██║  ██║██║██║ ╚████║   ██║  ██║╚██████╔╝██║ ╚═╝ ██║██║  ██║██║██║ ╚████║
 	╚═╝  ╚═╝ ╚═════╝ ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝   ╚═╝  ╚═╝ ╚═════╝ ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝
 
-	Build:   Mercredi le 26 août 2026 à 15 h 23
+	Build:   Mercredi le 26 août 2026 à 15 h 25
 	Author:  Maxime Larrivée-Roy <mlarriveeroy@gmail.com>
 	Github:  https://github.com/HumainHumain/
 	Website: https://humainhumain.com/
