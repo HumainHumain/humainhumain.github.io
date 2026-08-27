208c3007fbe579b8da5960f655cf3258722c702e
Maxime Larrivée-Roy <zmotrin@gmail.com>
2026-08-27 16:18:46 -0400

    Crédit graphisme

--- src/_layouts/footer.php
+++ src/_layouts/footer.php
@@ -3,6 +3,7 @@
     <footer>
         <div>© Tous droits réservés<br>Humain Humain, <?php echo date('Y'); ?></div>
         <div><small>Site web: <a noopener noreferrer target="_blank" id="credits-site-web" href="mailto:mlarriveeroy@gmail.com">Maxime Larrivée-Roy</a></small></div>
+        <div><small>Graphisme: <span class="fake-link">Karo-Otto von Oertzen</span></small></div>
         <div><small>Crédits photo: <a noopener noreferrer target="_blank" href="http://www.jlafreniere.ca">Joannie Lafrenière</a></small></div>
         <div>
             <a noopener noreferrer target="_blank" href="https://www.facebook.com/humainhumainconsultation.ethnographie/">
