d3dd2d0862000f6110a661f70e564c3209b85dbb
Maxime Larrivée-Roy <zmotrin@gmail.com>
2026-08-28 19:55:36 -0400

    Footer + Codepen

--- src/_layouts/footer.php
+++ src/_layouts/footer.php
@@ -2,7 +2,7 @@
     </main>
     <footer>
         <div>© Tous droits réservés<br>Humain Humain, <?php echo date('Y'); ?></div>
-        <div><small>Site web: <a noopener noreferrer target="_blank" id="credits-site-web" href="mailto:mlarriveeroy@gmail.com">Maxime Larrivée-Roy</a></small></div>
+        <div><small>Site web: <a noopener noreferrer target="_blank" id="credits-site-web" href="https://codepen.io/ZmotriN">Maxime Larrivée-Roy</a></small></div>
         <div><small>Graphisme: <a noopener noreferrer target="_blank" href="https://www.instagram.com/karrrlo">Karl-Otto von Oertzen</a></small></div>
         <div><small>Crédit photo: <a noopener noreferrer target="_blank" href="http://www.jlafreniere.ca">Joannie Lafrenière</a></small></div>
         <div>
